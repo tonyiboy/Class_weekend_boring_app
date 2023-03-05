cc0c5b5aafffc3f310865a5dd40da8d8aca0db8e
tonyiboy <antoniocodinggithub@gmail.com>
2023-03-05 22:20:34 +0100

    Class weekend boring

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.querySelector('#check').addEventListener('click', check)
+
+function check() {
+
+  const day = document.querySelector('#day').value.toLowerCase()
+  
+  //Conditionals go here
+  if(day === "tuesday" || day === "thursday" ){
+    alert('CLASS DAY!')
+  }else if(day === "saturday" || day === "sunday"){
+    alert('Weekend!')
+  }else{
+    alert('BORRRRRRINNNNNGGGGG!!!')
+  }
+
+}
