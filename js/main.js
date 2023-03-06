bc45416d9105240cbeb9b66f0096cb2eed31ef09
tonyiboy <antoniocodinggithub@gmail.com>
2023-03-06 18:40:06 +0100

    adding answers on page

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 document.querySelector('#check').addEventListener('click', check)
-
+const showAnswer = document.querySelector('#placeToSee')
 function check() {
 
   const day = document.querySelector('#day').value.toLowerCase()
   
+  
   //Conditionals go here
   if(day === "tuesday" || day === "thursday" ){
     alert('CLASS DAY!')
+    showAnswer.innerText= 'CLASS DAY!'
   }else if(day === "saturday" || day === "sunday"){
+    showAnswer.innerText= 'Weekend!'
     alert('Weekend!')
   }else{
+    showAnswer.innerText= 'BORRRRRRINNNNNGGGGG!!!'
     alert('BORRRRRRINNNNNGGGGG!!!')
   }
 
